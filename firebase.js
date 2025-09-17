ad6c606663db504126e9b876b66dab34f50df14a
Poorna Sachinthana <poornasachinthana2241@gmail.com>
2025-09-17 11:46:00 +0530

    Updated

---
+++ firebase.js
@@ -0,0 +1,24 @@
+  const firebaseConfig = {
+
+    apiKey: "AIzaSyCT7eMYiAAvEXUCrLbbqlsZeL5UvA5NePo",
+
+    authDomain: "to-do-list-67e22.firebaseapp.com",
+
+    projectId: "to-do-list-67e22",
+
+    storageBucket: "to-do-list-67e22.firebasestorage.app",
+
+    messagingSenderId: "84399797678",
+
+    appId: "1:84399797678:web:585d775ce420cf00ed043f",
+
+    measurementId: "G-98QGC912HV"
+
+  };
+
+
+  // Initialize Firebase
+
+  const app = initializeApp(firebaseConfig);
+
+  const analytics = getAnalytics(app);
